65b65d507668b7b94b2e762c1e0adb198c0e2866
Danil Mirosh <danilmirosh@iMac-Danil.local>
2022-09-04 09:27:32 +0500

    add setup/models

--- project/setup/api/models.py
+++ project/setup/api/models.py
@@ -6,3 +6,29 @@ genre: Model = api.model('Жанр', {
     'id': fields.Integer(required=True, example=1),
     'name': fields.String(required=True, max_length=100, example='Комедия'),
 })
+
+director: Model = api.model('Режиссеры', {
+    'id': fields.Integer(required=True, example=1),
+    'name': fields.String(required=True, max_length=100, example='Комедия'),
+})
+
+movie: Model = api.model('Фильмы', {
+    'id': fields.Integer(required=True, example=1),
+    'name': fields.String(required=True, max_length=100, example='Комедия'),
+    'title': fields.String(required=True, max_length=100, example='Комедия'),
+    'description': fields.String(required=True, max_length=100, example='Комедия'),
+    'trailer': fields.String(required=True, max_length=100, example='Комедия'),
+    'year': fields.Integer(required=True, example=100),
+    'rating': fields.Float(required=True, max_length=100),
+    'genre': fields.Nested(genre),
+    'director': fields.Nested(director)
+})
+
+user: Model = api.model('Пользователи', {
+    'id': fields.Integer(required=True, example=1),
+    'email': fields.String(required=True, max_length=100, example='Комедия'),
+    'password': fields.String(required=True, max_length=100, example='Комедия'),
+    'name': fields.String(required=True, max_length=100, example='Комедия'),
+    'surname': fields.String(required=True, max_length=100, example='Комедия'),
+    'favorite_genre': fields.Nested(genre)
+})
